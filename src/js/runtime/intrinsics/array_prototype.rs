430766ee62c14b55e86b82b690a364d921b62e39
Hans Halverson <hans_halverson@alumni.brown.edu>
2023-01-27 22:31:26 -0800

    [runtime] Implement Array.prototype.join and Array.prototype.push

--- src/js/runtime/intrinsics/array_prototype.rs
+++ src/js/runtime/intrinsics/array_prototype.rs
@@ -1,13 +1,19 @@
 use crate::{
     js::runtime::{
+        abstract_operations::{length_of_array_like, set},
         array_object::ArrayObject,
         builtin_function::BuiltinFunction,
+        error::type_error_,
+        function::get_argument,
         gc::Gc,
+        get,
+        numeric_constants::MAX_SAFE_INTEGER_U64,
         object_value::{Object, ObjectValue},
         ordinary_object::ordinary_object_create,
         property::Property,
         property_key::PropertyKey,
         realm::Realm,
+        to_string,
         type_utilities::to_object,
         Context, EvalResult, Value,
     },
@@ -41,7 +47,9 @@ impl ArrayPrototype {
 
         // Constructor property is added once ArrayConstructor has been created
         object.intrinsic_func(cx, &cx.names.entries(), Self::entries, 0, realm);
+        object.intrinsic_func(cx, &cx.names.join(), Self::join, 1, realm);
         object.intrinsic_func(cx, &cx.names.keys(), Self::keys, 0, realm);
+        object.intrinsic_func(cx, &cx.names.push(), Self::push, 0, realm);
         object.intrinsic_data_prop(&cx.names.values(), values_function);
 
         // 23.1.3.34 Array.prototype [ @@iterator ]
@@ -62,6 +70,41 @@ impl ArrayPrototype {
         ArrayIterator::new(cx, object, ArrayIteratorKind::KeyAndValue).into()
     }
 
+    // 23.1.3.16 Array.prototype.join
+    fn join(
+        cx: &mut Context,
+        this_value: Value,
+        arguments: &[Value],
+        _: Option<Gc<ObjectValue>>,
+    ) -> EvalResult<Value> {
+        let object = maybe!(to_object(cx, this_value));
+        let length = maybe!(length_of_array_like(cx, object));
+
+        let separator = get_argument(arguments, 0);
+        let separator = if separator.is_undefined() {
+            cx.get_interned_string(",")
+        } else {
+            maybe!(to_string(cx, separator))
+        };
+
+        let mut joined = String::new();
+
+        for i in 0..length {
+            if i > 0 {
+                joined.push_str(separator.str());
+            }
+
+            let element = maybe!(get(cx, object, &PropertyKey::from_u64(i)));
+
+            if !element.is_nullish() {
+                let next = maybe!(to_string(cx, element));
+                joined.push_str(next.str());
+            }
+        }
+
+        cx.heap.alloc_string(joined).into()
+    }
+
     // 23.1.3.17 Array.prototype.keys
     fn keys(
         cx: &mut Context,
@@ -71,6 +114,32 @@ impl ArrayPrototype {
     ) -> EvalResult<Value> {
         let object = maybe!(to_object(cx, this_value));
         ArrayIterator::new(cx, object, ArrayIteratorKind::Key).into()
+    }
+
+    // 23.1.3.21 Array.prototype.push
+    fn push(
+        cx: &mut Context,
+        this_value: Value,
+        arguments: &[Value],
+        _: Option<Gc<ObjectValue>>,
+    ) -> EvalResult<Value> {
+        let object = maybe!(to_object(cx, this_value));
+        let length = maybe!(length_of_array_like(cx, object));
+
+        let new_length = length + arguments.len() as u64;
+        if new_length > MAX_SAFE_INTEGER_U64 {
+            return type_error_(cx, "index is too large");
+        }
+
+        for (i, argument) in arguments.iter().enumerate() {
+            let key = PropertyKey::from_u64(length + i as u64);
+            maybe!(set(cx, object, &key, *argument, true));
+        }
+
+        let new_length_value = Value::from_u64(new_length);
+        maybe!(set(cx, object, &cx.names.length(), new_length_value, true));
+
+        new_length_value.into()
     }
 
     // 23.1.3.33 Array.prototype.values
--- src/js/runtime/numeric_constants.rs
+++ src/js/runtime/numeric_constants.rs
@@ -1,6 +1,10 @@
+// Max safe integer has magnitude 2^53 - 1
+
 pub const MAX_SAFE_INTEGER_F64: f64 = 9007199254740991.0;
 pub const MIN_SAFE_INTEGER_F64: f64 = -9007199254740991.0;
 
 pub const MIN_POSITIVE_SUBNORMAL_F64: f64 = 5e-324;
 
 pub const MAX_U32_AS_F64: f64 = u32::MAX as f64;
+
+pub const MAX_SAFE_INTEGER_U64: u64 = 9007199254740991;

7fe5a02d1d22efa8dac1fac2a59afe1dd17d5cef
Hans Halverson <hans_halverson@alumni.brown.edu>
2024-01-06 12:14:32 -0800

    [test] Add bytecode snapshot test runner

--- tests/snapshot_tests.rs
+++ tests/snapshot_tests.rs
@@ -1,11 +1,15 @@
-use brimstone::js;
+use brimstone::js::{
+    common::options::Options,
+    parser,
+    runtime::{bytecode::generator::BytecodeProgramGenerator, Context},
+};
 
 use std::cmp::min;
 use std::path::Path;
 use std::rc::Rc;
 use std::{env, error, fs};
 
-type GenericResult = Result<(), Box<dyn error::Error>>;
+type GenericResult<T> = Result<T, Box<dyn error::Error>>;
 
 const RECORD_ENV_VAR: &str = "RECORD";
 
@@ -14,12 +18,58 @@ struct TestEnv {
 }
 
 #[test]
-fn js_parser_snapshot_tests() -> GenericResult {
-    // Find path of parser tests directory
+fn js_parser_snapshot_tests() -> GenericResult<()> {
     let parser_tests_dir = Path::new(file!()).parent().unwrap().join("js_parser");
+    run_snapshot_tests(&parser_tests_dir, &mut |path| print_ast(path))
+}
 
+fn print_ast(path: &str) -> GenericResult<String> {
+    let parse_result = parse_script_or_module(path)?;
+    Ok(parser::print_program(&parse_result.program, &parse_result.program.source))
+}
+
+#[test]
+fn js_bytecode_snapshot_tests() -> GenericResult<()> {
+    // Context is shared between all tests
+    let options = Rc::new(Options::default());
+    let (cx, _) = Context::new(options, |_| ());
+
+    let bytecode_tests_dir = Path::new(file!()).parent().unwrap().join("js_bytecode");
+    run_snapshot_tests(&bytecode_tests_dir, &mut |path| print_bytecode(cx, path))
+}
+
+fn print_bytecode(cx: Context, path: &str) -> GenericResult<String> {
+    let mut parse_result = parse_script_or_module(path)?;
+    let source = parse_result.program.source.clone();
+    parser::analyze::analyze(&mut parse_result, source)?;
+
+    let bytecode_program =
+        BytecodeProgramGenerator::generate_from_program_parse_result(cx, &Rc::new(parse_result))?;
+
+    Ok(bytecode_program.debug_print_recursive())
+}
+
+fn parse_script_or_module(path: &str) -> GenericResult<parser::parser::ParseProgramResult> {
+    let source = Rc::new(parser::source::Source::new_from_file(path)?);
+
+    let parse_result = if path.contains("module") {
+        parser::parse_module(&source)?
+    } else {
+        parser::parse_script(&source)?
+    };
+
+    Ok(parse_result)
+}
+
+/// Run snapshot tests for all js files under the given directory.
+///
+/// Individual tests implemeneted as a function takes in a path and returns the output of the test.
+fn run_snapshot_tests(
+    root_path: &Path,
+    test_fn: &mut impl FnMut(&str) -> GenericResult<String>,
+) -> GenericResult<()> {
     let mut env = TestEnv { errors: vec![] };
-    visit_directory(&mut env, &parser_tests_dir)?;
+    visit_directory(&mut env, root_path, test_fn)?;
 
     if !env.errors.is_empty() {
         assert_eq!("actual", "expected", "\n{}\n", env.errors.join(""))
@@ -29,16 +79,20 @@ fn js_parser_snapshot_tests() -> GenericResult {
 }
 
 /// Recursively visit all subdirectories under the target directory, searching for js files.
-fn visit_directory(env: &mut TestEnv, path: &Path) -> GenericResult {
+fn visit_directory(
+    env: &mut TestEnv,
+    path: &Path,
+    test_fn: &mut impl FnMut(&str) -> GenericResult<String>,
+) -> GenericResult<()> {
     for entry in fs::read_dir(path)? {
         let entry = entry?;
         let path = entry.path();
         if path.is_dir() {
-            visit_directory(env, &path)?
+            visit_directory(env, &path, test_fn)?
         } else if path.is_file() {
             match path.extension() {
                 Some(extension) => match extension.to_str() {
-                    Some("js") => process_snapshot_test_file(env, &path)?,
+                    Some("js") => process_snapshot_test_file(env, &path, test_fn)?,
                     _ => (),
                 },
                 _ => (),
@@ -50,19 +104,15 @@ fn visit_directory(env: &mut TestEnv, path: &Path) -> GenericResult {
 }
 
 /// Compare acutal vs expected output for the test file with the given path.
-fn process_snapshot_test_file(env: &mut TestEnv, path: &Path) -> GenericResult {
+fn process_snapshot_test_file(
+    env: &mut TestEnv,
+    path: &Path,
+    test_fn: &mut impl FnMut(&str) -> GenericResult<String>,
+) -> GenericResult<()> {
     let path_str = path.to_str().unwrap();
     let exp_path = path.with_extension("exp");
 
-    let source = Rc::new(js::parser::source::Source::new_from_file(path_str)?);
-
-    let parse_result = if path_str.contains("module") {
-        js::parser::parse_module(&source)?
-    } else {
-        js::parser::parse_script(&source)?
-    };
-
-    let actual = js::parser::print_program(&parse_result.program, &source);
+    let actual = test_fn(path_str)?;
 
     let expected = if exp_path.exists() {
         fs::read_to_string(&exp_path)?

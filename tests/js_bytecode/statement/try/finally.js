386820c1149a9320025a3bbf4063b72fbbb39c1a
Hans Halverson <hans_halverson@alumni.brown.edu>
2024-02-27 22:57:50 -0800

    [tests] Add bytecode tests for finally with all possible abnormal paths

--- tests/js_bytecode/statement/try/finally.js
+++ tests/js_bytecode/statement/try/finally.js
@@ -58,3 +58,20 @@ function nested() {
   }
   6;
 }
+
+function allBranches(p1, p2, p3) {
+  while (true) {
+    try {
+      if (p1) {
+        break;
+      } else if (p2) {
+        continue;
+      } else if (p3) {
+        return 1;
+      }
+    } finally {
+      2;
+    }
+    3;
+  }
+}

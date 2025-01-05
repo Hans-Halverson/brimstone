8d56a9fd1ed5084c34ac466c86602d1fdf6f4fa2
Hans Halverson <hans_halverson@alumni.brown.edu>
2025-01-05 11:17:34 -0800

    [tests] Upgrade to latest test262

--- tests/test262/install_test262.sh
+++ tests/test262/install_test262.sh
@@ -6,8 +6,8 @@ CURRENT_DIR=$(cd "$(dirname "$0")" && pwd)
 TEST262_REPO_DIR="$CURRENT_DIR/test262"
 
 # Pinned commit hash of test262 repo that we test off of. Be sure to also update README.md.
-# Last updated on 2024-08-27
-TEST262_COMMIT_SHA="b69e9d5e722291dcb6ada5582c71887133626c63"
+# Last updated on 2025-01-05
+TEST262_COMMIT_SHA="8296db887368bbffa3379f995a1e628ddbe8421f"
 
 # Clone the test262 repo if it is not already present
 if [ ! -d "$TEST262_REPO_DIR" ]; then

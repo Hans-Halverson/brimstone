3083cf31d4a2f0aa3b9775df599c74b42748920c
Hans Halverson <hansghalverson@gmail.com>
2025-02-13 01:56:48 -0800

    [runtime] Implement Iterator.prototype.{filter, flatMap, map}, completing iterator helpers proposal (#131)
    ## Summary
    
    Finish implementing the iterator helpers proposal
    (https://github.com/tc39/proposal-iterator-helpers). The remaining
    methods to implement are `Iterator.prototype.{filter, flatMap, map}`
    which all use the new iterator helper objects introduced in
    https://github.com/Hans-Halverson/brimstone/pull/130.
    
    With this PR all stage 4 proposals as of the current day (2/13/2025)
    have been implemented except for `SharedArrayBuffer` and `Atomics`.
    
    ## Tests
    
    Stop ignoring all test262 iterator helpers tests. All test262 iterator
    helpers tests pass.

--- src/js/runtime/intrinsics/iterator_prototype.rs
+++ src/js/runtime/intrinsics/iterator_prototype.rs
@@ -36,8 +36,11 @@ impl IteratorPrototype {
 
         object.intrinsic_func(cx, cx.names.drop(), Self::drop, 1, realm);
         object.intrinsic_func(cx, cx.names.every(), Self::every, 1, realm);
+        object.intrinsic_func(cx, cx.names.filter(), Self::filter, 1, realm);
         object.intrinsic_func(cx, cx.names.find(), Self::find, 1, realm);
+        object.intrinsic_func(cx, cx.names.flat_map(), Self::flat_map, 1, realm);
         object.intrinsic_func(cx, cx.names.for_each(), Self::for_each, 1, realm);
+        object.intrinsic_func(cx, cx.names.map_(), Self::map, 1, realm);
         object.intrinsic_func(cx, cx.names.reduce(), Self::reduce, 1, realm);
         object.intrinsic_func(cx, cx.names.some(), Self::some, 1, realm);
         object.intrinsic_func(cx, cx.names.take(), Self::take, 1, realm);
@@ -175,6 +178,31 @@ impl IteratorPrototype {
         }
     }
 
+    /// Iterator.prototype.filter (https://tc39.es/ecma262/#sec-iterator.prototype.filter)
+    pub fn filter(
+        cx: Context,
+        this_value: Handle<Value>,
+        arguments: &[Handle<Value>],
+        _: Option<Handle<ObjectValue>>,
+    ) -> EvalResult<Handle<Value>> {
+        if !this_value.is_object() {
+            return type_error(cx, "Iterator.prototype.filter called on non-object");
+        }
+
+        // Verify the predicate argument is a function, closing the underlying iterator if not
+        let predicate_arg = get_argument(cx, arguments, 0);
+        if !is_callable(predicate_arg) {
+            let error =
+                type_error_value(cx, "Iterator.prototype.filter predicate is not a function");
+            return iterator_close(cx, this_value.as_object(), Err(error));
+        }
+        let predicate = predicate_arg.as_object();
+
+        // Get the underlying iterator and create a new iterator helper map object
+        let iterated = get_iterator_direct(cx, this_value.as_object())?;
+        Ok(IteratorHelperObject::new_filter(cx, &iterated, predicate).as_value())
+    }
+
     /// Iterator.prototype.find (https://tc39.es/ecma262/#sec-iterator.prototype.find)
     pub fn find(
         cx: Context,
@@ -220,6 +248,30 @@ impl IteratorPrototype {
         }
     }
 
+    /// Iterator.prototype.flatMap (https://tc39.es/ecma262/#sec-iterator.prototype.flatmap)
+    pub fn flat_map(
+        cx: Context,
+        this_value: Handle<Value>,
+        arguments: &[Handle<Value>],
+        _: Option<Handle<ObjectValue>>,
+    ) -> EvalResult<Handle<Value>> {
+        if !this_value.is_object() {
+            return type_error(cx, "Iterator.prototype.flatMap called on non-object");
+        }
+
+        // Verify the mapper argument is a function, closing the underlying iterator if not
+        let mapper_arg = get_argument(cx, arguments, 0);
+        if !is_callable(mapper_arg) {
+            let error = type_error_value(cx, "Iterator.prototype.flatMap mapper is not a function");
+            return iterator_close(cx, this_value.as_object(), Err(error));
+        }
+        let mapper = mapper_arg.as_object();
+
+        // Get the underlying iterator and create a new iterator helper map object
+        let iterated = get_iterator_direct(cx, this_value.as_object())?;
+        Ok(IteratorHelperObject::new_flat_map(cx, &iterated, mapper).as_value())
+    }
+
     /// Iterator.prototype.forEach (https://tc39.es/ecma262/#sec-iterator.prototype.foreach)
     pub fn for_each(
         cx: Context,
@@ -261,6 +313,30 @@ impl IteratorPrototype {
                 Ok(_) => counter += 1,
             }
         }
+    }
+
+    /// Iterator.prototype.map (https://tc39.es/ecma262/#sec-iterator.prototype.map)
+    pub fn map(
+        cx: Context,
+        this_value: Handle<Value>,
+        arguments: &[Handle<Value>],
+        _: Option<Handle<ObjectValue>>,
+    ) -> EvalResult<Handle<Value>> {
+        if !this_value.is_object() {
+            return type_error(cx, "Iterator.prototype.map called on non-object");
+        }
+
+        // Verify the mapper argument is a function, closing the underlying iterator if not
+        let mapper_arg = get_argument(cx, arguments, 0);
+        if !is_callable(mapper_arg) {
+            let error = type_error_value(cx, "Iterator.prototype.map mapper is not a function");
+            return iterator_close(cx, this_value.as_object(), Err(error));
+        }
+        let mapper = mapper_arg.as_object();
+
+        // Get the underlying iterator and create a new iterator helper map object
+        let iterated = get_iterator_direct(cx, this_value.as_object())?;
+        Ok(IteratorHelperObject::new_map(cx, &iterated, mapper).as_value())
     }
 
     /// Iterator.prototype.reduce (https://tc39.es/ecma262/#sec-iterator.prototype.reduce)

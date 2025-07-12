5298e0714452df6f557e9a32749eccf0f674a702
Hans Halverson <hansghalverson@gmail.com>
2025-07-11 19:33:14 -0700

    [bytecode] Store intermediate object or array in temporary while building to avoid clobbering observable register

--- tests/js_bytecode/expression/array.js
+++ tests/js_bytecode/expression/array.js
@@ -25,3 +25,15 @@ function onlySpread(p) {
 function multipleSpreads(p1, p2, p3) {
   return [...p1, ...p2, 1, ...p3];
 }
+
+function temporaryRegisterToAvoidClobbering() {
+  var local = 1;
+  // Intermediate array is written to a temporary to avoid clobbering
+  local = [ 2 ];
+}
+
+function noTemporaryRegisterNeededForEmptyObject() {
+  var local = 1;
+  // Intermediate array is not needed if array is empty since no observable clobbering can occur
+  local = [];
+}

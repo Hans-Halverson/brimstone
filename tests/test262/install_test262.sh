8429a633d41337d9997f6693291e58b6e32f654a
Hans Halverson <hansghalverson@gmail.com>
2025-07-12 12:38:45 -0700

    [test] Bump test262 to latest version

--- tests/test262/install_test262.sh
+++ tests/test262/install_test262.sh
@@ -6,8 +6,8 @@ CURRENT_DIR=$(cd "$(dirname "$0")" && pwd)
 TEST262_REPO_DIR="$CURRENT_DIR/test262"
 
 # Pinned commit hash of test262 repo that we test off of. Be sure to also update README.md.
-# Last updated on 2025-02-07
-TEST262_COMMIT_SHA="bc5c14176e2b11a78859571eb693f028c8822458"
+# Last updated on 2025-07-12
+TEST262_COMMIT_SHA="f0dc15c6c7ec095ba3caf3acc0f8665394665841"
 
 # Clone the test262 repo if it is not already present
 if [ ! -d "$TEST262_REPO_DIR" ]; then

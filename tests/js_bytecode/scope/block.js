aa37bbf198ef83e1957968a2a0f9181770e6f688
Hans Halverson <hansghalverson@gmail.com>
2025-07-12 00:06:19 -0700

    [bytecode] Fix scope exit at end of block statement with abrupt completion

--- tests/js_bytecode/scope/block.js
+++ tests/js_bytecode/scope/block.js
@@ -17,3 +17,55 @@ function captures() {
     }
   }
 }
+
+let outerScope = 1;
+
+function abruptEndReturn() {
+  block: {
+    let x = 1;
+    (() => x);
+
+    // Force a path that leaves the block
+    if (true) {
+      break block;
+    }
+
+    // Abrupt (return) completion
+    return;
+  }
+
+  // Correct outer scope
+  outerScope;
+}
+
+
+function abruptEndThrow() {
+  block: {
+    let x = 1;
+    (() => x);
+
+    // Force a path that leaves the block
+    if (true) {
+      break block;
+    }
+
+    // Abrupt (throw) completion
+    throw 2;
+  }
+
+  // Correct outer scope
+  outerScope;
+}
+
+function abruptEndBreak() {
+  block: {
+    let x = 1;
+    (() => x);
+
+    // Abrupt (break) completion
+    break block;
+  }
+
+  // Correct outer scope
+  outerScope;
+}

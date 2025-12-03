9347892aa0b5c424215de4ed3143ac83cda4e87c
Hans Halverson <hansghalverson@gmail.com>
2025-12-03 01:59:33 -0800

    [runtime] Implement RegExp.prototype.compile

--- src/js/runtime/intrinsics/mod.rs
+++ src/js/runtime/intrinsics/mod.rs
@@ -54,7 +54,7 @@ pub mod promise_prototype;
 pub mod proxy_constructor;
 mod reflect_object;
 pub mod regexp_constructor;
-mod regexp_prototype;
+pub mod regexp_prototype;
 pub mod regexp_string_iterator;
 pub mod rust_runtime;
 pub mod set_constructor;

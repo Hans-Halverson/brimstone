6bb3307e9d03d9f9194a9f1d58f08803fd20efe8
Hans Halverson <hansghalverson@gmail.com>
2026-06-09 12:45:29 -0700

    [regexp] Fix duplicate named capture group checks within nested terms (#323)
    ## Summary
    
    Duplicate named capture groups are allowed in separate alternatives. The
    system for checking for valid/invalid cases of this had a major bug - we
    were not handling named capture groups nested within other terms (e.g.
    within groups, quantifiers, lookaround).
    
    The bug was that we were clearing the names from the current scope
    between alternatives and then dropping them at the end of
    `parse_disjuction`, when we actually need to be adding names present in
    *any* alternative to the scope at the end of the disjunction.
    
    ## Tests
    
    - Added integration tests with a variety of tests for whether duplicate
    named captures in nested situations throws an error
    - Added example error snapshot tests for simple cases affected by this
    bug

---
+++ tests/integration/built-ins/RegExp/duplicate_named_capture_groups.js
@@ -0,0 +1,45 @@
+/*---
+description: ^
+  Duplicate named capture group may be used in separate alternatives. Detect cases where the named
+  capture groups appear in nested structures and make sure they are properly detected.
+---*/
+
+// Valid cases:
+
+new RegExp("(?<x>a)|(?<x>b)");
+new RegExp("(?<x>a)|(?<x>b)|(?<x>c)");
+new RegExp("(?:(?<x>a)|(?<x>b))");
+new RegExp("(?<y>(?<x>a)|(?<x>b))");
+new RegExp("(?:(?<x>a))|(?:(?<x>b))");
+new RegExp("(?<x>a)b|c(?<x>d)");
+new RegExp("((?<x>a)|(?<x>b))");
+new RegExp("(?<x>a)*|(?<x>b)*");
+
+// Invalid cases:
+
+// Simple named capture groups in sequence
+assert.throws(SyntaxError, () => new RegExp("(?<x>a)(?<x>b)"));
+assert.throws(SyntaxError, () => new RegExp("(?<x>a).(?<x>b)"));
+assert.throws(SyntaxError, () => new RegExp("(?<x>a)(?<x>b)(?<x>c)"));
+
+// Nested within capture groups
+assert.throws(SyntaxError, () => new RegExp("(?:(?<x>a))(?<x>b)"));
+assert.throws(SyntaxError, () => new RegExp("(?<x>a)(?:(?<x>b))"));
+assert.throws(SyntaxError, () => new RegExp("(?<y>(?<x>a))(?<x>b)"));
+assert.throws(SyntaxError, () => new RegExp("(?:(?:(?<x>a)))(?<x>b)"));
+assert.throws(SyntaxError, () => new RegExp("(?:(?<x>a)|(?<x>b))(?<x>c)"));
+
+// Nested within capture groups of the same name
+assert.throws(SyntaxError, () => new RegExp("(?<x>(?<x>a))"));
+assert.throws(SyntaxError, () => new RegExp("(?<x>(?<x>a)|b)"));
+
+// Named groups within lookarounds
+assert.throws(SyntaxError, () => new RegExp("(?=(?<x>a))(?<x>b)"));
+assert.throws(SyntaxError, () => new RegExp("(?<x>a)(?=(?<x>b))"));
+assert.throws(SyntaxError, () => new RegExp("(?!(?<x>a))(?<x>b)"));
+assert.throws(SyntaxError, () => new RegExp("(?<=(?<x>a))(?<x>b)"));
+
+// Named groups within quantifiers
+assert.throws(SyntaxError, () => new RegExp("(?<x>a)*(?<x>b)"));
+assert.throws(SyntaxError, () => new RegExp("(?:(?<x>a))*(?<x>b)"));
+assert.throws(SyntaxError, () => new RegExp("(?:(?<x>a)|(?<x>b))+(?<x>c)"));

7d88f2dd042c67e26c0e9cce992628c4f6c149a5
Hans Halverson <hans_halverson@alumni.brown.edu>
2026-06-10 12:28:20 -0700

    [regexp] First optional repetition of quantifier cannot match empty string

---
+++ tests/integration/built-ins/RegExp/quantifier_empty_repetitions.js
@@ -0,0 +1,28 @@
+/*---
+description: Optional repetitions of a quantifier cannot match the empty string.
+---*/
+
+// Minimal repros
+assert.compareArray(/(x*?)?/.exec('x'), ['x', 'x']);
+assert.compareArray(/(x?|c)?/.exec('cc'), ['c', 'c']);
+
+// Optional inlined repetitions without min
+assert.compareArray(/(x?|c){0,3}/.exec('ccc'), ['ccc', 'c']);
+
+// Optional loop repetitions without min
+assert.compareArray(/(x?|c){0,20}/.exec('c'.repeat(20)), ['cccccccccccccccccccc','c']);
+
+// Required inline repetitions followed by optional inline repetitions
+assert.compareArray(/(x?|c){2,4}/.exec('cccc'), ['cc','c']);
+
+// Required inline repetitions followed by optional loop repetitions
+assert.compareArray(/(x?|c){1,15}/.exec('c'.repeat(15)), ['cccccccccccccc','c']);
+
+// Required loop repetitions followed by optional inlined repetitions
+assert.compareArray(/(x?|c){11,13}/.exec('c'.repeat(13)), ['cc', 'c']);
+
+// Required loop repetitions followed by optional loop repetitions
+assert.compareArray(/(x?|c){15,30}/.exec('c'.repeat(30)), ['ccccccccccccccc', 'c']);
+
+// Required loop repetitions followed by unbounded optional repetitions
+assert.compareArray(/(x?|c){2,}/.exec('c'.repeat(10)), ['cccccccccc', 'c']);

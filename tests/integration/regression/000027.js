3d71928b979da7fb0c53716a4b886dc6a880bbda
Hans Halverson <hansghalverson@gmail.com>
2026-06-11 18:27:53 -0700

    [regexp] Ensure the comparison register is always cleared after use, even when backtracking (#347)
    ## Summary
    
    The RegExp matching engine keeps a comparison register used for code
    point set comparisons in sequence. This was is reset after being used by
    instructions that end a comparison sequence.
    
    There was a bug where when the end of the backtrack stack is hit we
    would early return instead of clearing the comparison register. Normally
    this is irrelevant since we would exit the entire matching process at
    that point. But if it happens in a lookaround then the engine will
    continue to execute, and the old comparison register value could
    influence future comparisons.
    
    We can fix this by refactoring to always "consume" the comparison
    register when accessed so that we always reset it during the final
    comparison instruction.
    
    ## Tests
    
    - Added regression test with cases that were previously failing (code
    point set comparisons and both word boundary assertions could cause the
    leak)

---
+++ tests/integration/regression/000027.js
@@ -0,0 +1,13 @@
+/*---
+description: ^
+  Ensure the RegExp comparison register is cleared after a failed lookaround so it does not leak to
+  affect another match.
+---*/
+
+// Code point set checks could leak the comparison register
+assert.sameValue(/(?![^z])z[^z]/.exec("zc")[0], "zc");
+assert.sameValue(/(?![^z])z[a-c]/.exec("zq"), null);
+
+// Word boundary assertions could leak the comparison register
+assert.sameValue(/a(?!\b)[^q]/.exec("ab")[0], "ab");
+assert.sameValue(/a(?!\B)[^q]/.exec("a.")[0], "a.");

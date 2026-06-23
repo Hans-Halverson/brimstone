a347d44287351395b6dd498b0ba132f557faad60
Hans Halverson <hansghalverson@gmail.com>
2026-06-22 18:13:37 -0700

    [runtime] More fixes to String padding method validation order (#372)
    ## Summary
    
    More fixes to `String.prototype.{padStart, padEnd}` validation order
    missed in https://github.com/Hans-Halverson/brimstone/pull/371.
    
    - `ToString(fillString)` not performed if string already has requested
    max length
    - Never error for new string length if `fillString` is the empty string
    
    ## Tests
    
    - Added integration tests for new cases

--- tests/integration/built-ins/String/pad_start.js
+++ tests/integration/built-ins/String/pad_start.js
@@ -2,5 +2,12 @@
 description: String.prototype.padStart.
 ---*/
 
+// ToString(fillString) performed before validating new string length
 const poisoned = { toString() { throw new Test262Error(); } };
 assert.throws(Test262Error, () => 'a'.padStart(Infinity, poisoned));
+
+// ToString(fillString) not performed if string already has max length
+assert.sameValue('abcde'.padStart(3, poisoned), 'abcde');
+
+// Do not error for new string length if empty string is being added
+assert.sameValue('abc'.padStart(Infinity, ''), 'abc');
